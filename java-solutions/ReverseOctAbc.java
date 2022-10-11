68397f75aed48e89ccbb6a9a55fc9986ac64f382
Georgiy Korneev <kgeorgiy@kgeorgiy.info>
2022-10-11 14:49:51 +0300

    Checked by an instructor

--- java-solutions/ReverseOctAbc.java
+++ java-solutions/ReverseOctAbc.java
@@ -10,7 +10,7 @@ public class ReverseOctAbc {
     }
 
     private static char convert(char c) {
-    	return (char)((int)c - (int)'a' + (int)'0');
+        return (char)((int)c - (int)'a' + (int)'0');
     }
 
     private static int[] getNumbers(String line) {
@@ -21,64 +21,65 @@ public class ReverseOctAbc {
         int ptr = 0;
         StringBuilder num = new StringBuilder();
         for (int i = 0; i < line.length(); i++) {
-        	char c = line.charAt(i);
-        	boolean skip = !(check(c));
-        	if (!skip) {
-        		if ((int)'a' <= (int)c && (int)c <= (int)'j') {
-        			c = convert(c);
-        		}
-        		num.append(c);
-        	}
-        	if ((skip || i + 1 == line.length()) && num.length() > 0) {
-        		int base = 10;
-        		if (num.charAt(num.length() - 1) == 'o' || num.charAt(num.length() - 1) == 'O') {
-        			num.setLength(num.length() - 1);
-        			base = 8;
-        		}
-        		int number = (int)Long.parseLong(num.toString(), base);
-        		if (ptr >= numbers.length) {
-        			numbers = Arrays.copyOf(numbers, (numbers.length * 3 + 1) / 2);
-        		}
-        		numbers[ptr++] = number;
-        		num = new StringBuilder();
-        	}
+            char c = line.charAt(i);
+            boolean skip = !(check(c));
+            if (!skip) {
+                if ((int)'a' <= (int)c && (int)c <= (int)'j') {
+                    c = convert(c);
+                }
+                num.append(c);
+            }
+            if ((skip || i + 1 == line.length()) && num.length() > 0) {
+                int base = 10;
+                if (num.charAt(num.length() - 1) == 'o' || num.charAt(num.length() - 1) == 'O') {
+                    num.setLength(num.length() - 1);
+                    base = 8;
+                }
+                // :NOTE: parseLong
+                int number = (int) Long.parseLong(num.toString(), base);
+                if (ptr >= numbers.length) {
+                    numbers = Arrays.copyOf(numbers, (numbers.length * 3 + 1) / 2);
+                }
+                numbers[ptr++] = number;
+                num = new StringBuilder();
+            }
         }
         numbers = Arrays.copyOf(numbers, ptr);
         int[] reversed = new int[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
-        	reversed[i] = numbers[numbers.length - 1 - i];
+            reversed[i] = numbers[numbers.length - 1 - i];
         }
         return reversed;
     }
 
     public static void main(String[] args) {
-    	try {
-	        MyScanner sc = new MyScanner(new BufferedReader(
-											new InputStreamReader(System.in)));
-	        int[][] lines = new int[1][];
-	        int ptr = 0;
-	        try {
-		        while (sc.hasNextLine()) {
-		            String line = sc.nextLine();
-		            if (ptr >= lines.length) {
-		                lines = Arrays.copyOf(lines, (lines.length * 3 + 1) / 2);
-		            }
-		            lines[ptr++] = getNumbers(line);
-		        }
-		        for (int i = lines.length - 1; i >= 0; i--) {
-		            if (lines[i] != null) {
-		                for (int num : lines[i]) {
-		                    System.out.print(num + " ");
-		                }
-		                System.out.println("");
-		            }
-		        }
-		    }
-		    finally {
-		    	sc.close();
-		    }
-	    } catch (IOException e) {
-	    	System.out.println("An error occured while reading from input: " + e.getMessage());
-	    }
+        try {
+            MyScanner sc = new MyScanner(new BufferedReader(
+                                            new InputStreamReader(System.in)));
+            int[][] lines = new int[1][];
+            int ptr = 0;
+            try {
+                while (sc.hasNextLine()) {
+                    String line = sc.nextLine();
+                    if (ptr >= lines.length) {
+                        lines = Arrays.copyOf(lines, (lines.length * 3 + 1) / 2);
+                    }
+                    lines[ptr++] = getNumbers(line);
+                }
+                for (int i = lines.length - 1; i >= 0; i--) {
+                    if (lines[i] != null) {
+                        for (int num : lines[i]) {
+                            System.out.print(num + " ");
+                        }
+                        System.out.println("");
+                    }
+                }
+            }
+            finally {
+                sc.close();
+            }
+        } catch (IOException e) {
+            System.out.println("An error occured while reading from input: " + e.getMessage());
+        }
     }
 }

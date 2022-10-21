4cbd5415f33e08d8b4fd4b4f86695871586a245e
aww <vovapodkorytov966@gmail.com>
2022-10-22 00:37:45 +0300

    Scanner fixed

--- java-solutions/MyScanner.java
+++ java-solutions/MyScanner.java
@@ -14,21 +14,21 @@ public class MyScanner {
 	}
 
 	private int updateBuffer() throws IOException, IllegalStateException { // read buffer and return it's size if success, otherwise -1
-		if (this.closed) {
+		if (closed) {
 			throw new IllegalStateException("Scanner is already closed");
 		}
-		int read = this.reader.read(this.buffer);
-		this.bufferSize = read;
+		int read = reader.read(buffer);
+		bufferSize = read;
 		return read;
 	}
 
 	private boolean hasNextChar() throws IOException, IllegalStateException { // return true if there is a char left
-		if (this.bufferSize == -1) {
+		if (bufferSize == -1) {
 			return false;
 		}
-		if (this.index == this.bufferSize - 1) {
+		if (index == bufferSize - 1) {
 			if (this.updateBuffer() > 0) {
-				this.index = -1;
+				index = -1;
 				return true;
 			}
 			else {
@@ -46,20 +46,20 @@ public class MyScanner {
 		return this.hasNextChar();
 	}
 
-	private boolean checkForWord(char c) {
+	private static boolean checkForWord(char c) {
 		return (Character.isLetter(c) ||
 		 Character.getType(c) == Character.DASH_PUNCTUATION ||
 		  c == '\'');
 	}
 
-	private boolean checkForNumber(char c) {
+	private static boolean checkForNumber(char c) {
 		return (Character.isDigit(c) ||
                 ((int)'a' <= (int)c && (int)c <= (int)'j') ||
                 (c == '-' || c == '+') ||
                 (c == 'o' || c == 'O'));
 	}
 
-	private char convert(char c) {
+	private static char convert(char c) {
 		return (char)((int)c - (int)'a' + (int)'0');
 	}
 
@@ -69,15 +69,20 @@ public class MyScanner {
 			throw new NoSuchElementException("Can't read more tokens");
 		}
 		while (this.hasNextChar()) {
-			this.index++;
-			char c = this.buffer[this.index];
-			boolean ok = false;
+			index++;
+			char c = buffer[index];
+			
+			boolean ok = true;
 			if (type == 0) {
-				ok = checkForWord(c);
+				ok &= this.checkForWord(c);
 			}
-			else if (type == 1) {
-				ok = checkForNumber(c);
+			if (type == 1) {
+				ok &= this.checkForNumber(c);
 			}
+
+			// System.out.println("[" + c + " " + index + " " + ok + "]");
+			// System.out.println(Character.isDigit(c) + " " + ((int)'a' <= (int)c && (int)c <= (int)'j') + " " + (c == '-' || c == '+') + " " + (c == 'o' || c == 'O'));
+			// System.out.println("---------------------");
 
 			if (ok) {
 				if (type == 1) {
@@ -88,31 +93,40 @@ public class MyScanner {
 				sb.append(c);
 			}
 			else {
-				return sb.toString();
+				// System.out.println("{" + sb.toString() + "}");
+				if (sb.length() > 0) {
+					return sb.toString();
+				}
+				sb = new StringBuilder();
 			}
 		}
+		// System.out.println("{" + sb.toString() + "}");
 		return sb.toString();
 	}
 
 	public String nextToken() throws IOException, IllegalStateException, NoSuchElementException {
+		// System.out.println("________________________________");
 		return this.next(0);
 	}
 
 	public int nextInt() throws NumberFormatException, IOException, IllegalStateException, NoSuchElementException {
 		String candidate = this.next(1).toLowerCase();
 		int radix = 10;
-		if (candidate.charAt(candidate.length() - 1) == 'o') {
+		if (candidate.length() > 0 && candidate.charAt(candidate.length() - 1) == 'o') {
 			candidate = candidate.substring(0, candidate.length() - 1);
 			radix = 8;
 		}
-		return Integer.parseInt(candidate, radix);
+		if (candidate.charAt(0) == '-') {
+			return Integer.parseInt(candidate, radix);
+		}
+		return Integer.parseUnsignedInt(candidate, radix);
 	}
 
 	private boolean isNextLine() throws IllegalStateException, IOException, NoSuchElementException {
-		if (this.buffer[this.index] == '\r') {
+		if (buffer[index] == '\r') {
 			if (this.hasNextChar()) {
-				this.index++;
-				if (this.buffer[this.index] == '\n') {
+				index++;
+				if (buffer[index] == '\n') {
 					// if (this.hasNextChar()) {
 					// 	this.index++;
 					// }
@@ -121,10 +135,10 @@ public class MyScanner {
 			}
 			return true;
 		}
-		else if (this.buffer[this.index] == '\n' ||
-					this.buffer[this.index] == '\u2028' ||
-					this.buffer[this.index] == '\u2029' ||
-					this.buffer[this.index] == '\u0085') {
+		else if (buffer[index] == '\n' ||
+					buffer[index] == '\u2028' ||
+					buffer[index] == '\u2029' ||
+					buffer[index] == '\u0085') {
 			// if (this.hasNextChar()) {
 			// 	this.index++;
 			// }
@@ -136,20 +150,20 @@ public class MyScanner {
 	public String nextLine() throws IllegalStateException, IOException, NoSuchElementException {
 		StringBuilder line = new StringBuilder();
 		while (this.hasNextChar()) {
-			this.index++;
+			index++;
 			if (this.isNextLine()) {
 				return line.toString();
 			}
-			line.append(this.buffer[this.index]);
+			line.append(buffer[index]);
 		}
 		return line.toString();
 	}
 
 	public void close() throws IOException, IllegalStateException {
-		if (this.closed) {
+		if (closed) {
 			throw new IllegalStateException("Scanner is already closed");
 		}
-		this.reader.close();
-		this.closed = true;
+		reader.close();
+		closed = true;
 	}
 }

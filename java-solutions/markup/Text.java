fcef235291a690748eeef4bb5c3f488917f80fc8
Nikolai Korobeinikov <ricnorr@yandex-team.ru>
2022-10-25 14:46:44 +0300

    Checked by an instructor

--- java-solutions/markup/Text.java
+++ java-solutions/markup/Text.java
@@ -11,3 +11,11 @@ public class Text extends AbstractText implements Markdown {
         super(txt, "", "");
     }
 }
+
+/**
+ *__current-repo/java-solutions/markup/MarkupListTest.java:22: error: duplicate class: markup.MarkupListTest
+ * public final class MarkupListTest {
+ *              ^
+ * 1 error
+ * ERROR: Compilation failed
+ */
